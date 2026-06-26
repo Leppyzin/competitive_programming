2f3068c047a7a77b3e7cb1dcbec577b94c36ddf4
[ - Lucas Modesto - ] <130197468+Leppyzin@users.noreply.github.com>
2026-06-26 17:51:45 -0300

    Update and rename Beecrowd - 1046 | Tempo de Jogo.md to Beecrowd - 1046 | Tempo de Jogo.c

---
+++ Aula_03/Exercícios Resolvidos/Beecrowd - 1046 | Tempo de Jogo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int inicio,fim,tempo;
+    scanf("%d%d",&inicio,&fim);
+    
+    if(inicio >= fim){
+        tempo = 24-inicio+fim;
+    } else {
+        tempo = fim-inicio;
+    }
+    
+    printf("O JOGO DUROU %d HORA(S)\n",tempo);
+
+    return 0;
+}
