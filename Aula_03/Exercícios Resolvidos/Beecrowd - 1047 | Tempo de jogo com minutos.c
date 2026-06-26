b769eea03a1e247feaf296cd489f90b250c9fadf
[ - Lucas Modesto - ] <130197468+Leppyzin@users.noreply.github.com>
2026-06-26 20:02:26 -0300

    Update and rename Beecrowd - 1047 | Tempo de jogo com minutos.md to Beecrowd - 1047 | Tempo de jogo com minutos.c

---
+++ Aula_03/Exercícios Resolvidos/Beecrowd - 1047 | Tempo de jogo com minutos.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int hi, mi, hf, mf, tempoh, tempom;
+
+    scanf("%d%d%d%d", &hi, &mi, &hf, &mf);
+
+    if (hf > hi) {
+        tempoh = hf - hi;
+    } else if (hf < hi) {
+        tempoh = hf - hi + 24;
+    } else { // hf == hi
+        tempoh = 24;
+    }
+
+    if (mf >= mi) {
+        tempom = mf - mi;
+    } else {
+        tempom = 60 + mf - mi;
+        tempoh--;  
+    }
+
+
+    if(tempoh == 24){
+        tempoh = 0;
+    }
+
+    if (tempoh == 0 && tempom == 0){
+        tempoh = 24;
+    }
+    
+    
+    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", tempoh, tempom);
+
+    return 0;
+}
